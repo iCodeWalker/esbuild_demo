afe011db7b43a51858ffbdef8ede94ccc3a52ef3
vaibhavkadwey <vaibhav3kadwey@gmail.com>
2023-02-16 15:35:33 +0530

    added caching of files

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,5 +1,12 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
+import localforage from 'localforage';
+
+// create a new object that we can use to interact with instance of an index database inside the browser.
+
+const fileCache = localforage.createInstance({
+    name: 'filecache'
+})
  
 export const unpkgPathPlugin = () => {
   return {
@@ -39,16 +46,38 @@ export const unpkgPathPlugin = () => {
               loader: 'jsx',
               contents: `
                 import message from 'nested-test-pkg';
-                console.log(message);
+                import react from 'react';
+                import axios from 'axios';
+                console.log(message, react, axios);
               `,
             };
         } 
+
+        // Check to see if we already have fetched file and it is in the cache data.
+        const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+
+        // if it is, return it.
+
+        if(cachedResult) {
+            return cachedResult;
+        }
+
         const {data ,request} = await axios.get(args.path);
-        return {
+
+        const result: esbuild.OnLoadResult =  {
             loader: 'jsx',
             contents: data,
             resolveDir: new URL('./', request.responseURL).pathname
         }
+
+        // ------ Store response in cache -------------
+        await fileCache.setItem(args.path, result);
+
+        return result;
+
+
+
+
         // if (args.path === 'index.js') {
         //   return {
         //     loader: 'jsx',
